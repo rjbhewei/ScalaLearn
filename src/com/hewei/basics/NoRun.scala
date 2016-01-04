408a96b100aca121a22d92d3228e09eaebb10f0f
rjbhewei <rjbhewei@gmail.com>
2016-01-04 14:47:54 +0800

    basics test

---
+++ src/com/hewei/basics/NoRun.scala
@@ -0,0 +1,19 @@
+package com.hewei.basics
+
+/**
+  *
+  * @author hewei
+  *
+  * @date 16/1/4  13:54
+  *
+  * @version 5.0
+  *
+  * @desc
+  *
+  */
+class NoRun {
+  def main(args: Array[String]) {
+    val two =1+1
+    print(two)
+  }
+}
