faf48c4821b5ec7c4a9b3b10497aa1a9c4936386
rjbhewei <rjbhewei@gmail.com>
2016-01-05 18:32:07 +0800

    closure test

--- src/com/hewei/closure/TestClosure.scala
+++ src/com/hewei/closure/TestClosure.scala
@@ -23,14 +23,24 @@ object TestClosure {
   def main(args: Array[String]) = {
     //    oncePerSecond()  //正常使用
     //    oncePerSecond(timeFlies) //闭包
-    oncePerSecond(() => println("xxooxxoo")) //匿名   下面需要使用fn()
+    //    oncePerSecond(() => println("xxooxxoo")) //匿名   下面需要使用fn()
+    oncePerSecond(1, () => println("xxooxxoo"))
   }
+
 
   def oncePerSecond(fn: () => Unit) {
     while (true) {
       //fn
       fn()
       Thread.sleep(1000)
+    }
+  }
+
+  def oncePerSecond(time: Long, fn: () => Unit) {
+    while (true) {
+      //fn
+      fn()
+      Thread.sleep(time)
     }
   }
 
