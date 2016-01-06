43c2898d3ce29aac121120a0a1f188a745189c4e
rjbhewei <rjbhewei@gmail.com>
2016-01-06 11:41:49 +0800

    contractor test

---
+++ src/com/hewei/contractor/ObjectContractor.scala
@@ -0,0 +1,20 @@
+package com.hewei.contractor
+
+
+/**
+  *
+  * @author hewei
+  *
+  * @date 16/1/6  11:37
+  *
+  * @version 5.0
+  *
+  * @desc
+  *
+  */
+object ObjectContractor {
+  def main(args: Array[String]) {
+    def a = new TestContractor(1, 2)
+    def b = new TestContractor()
+  }
+}
---
+++ src/com/hewei/contractor/TestContractor.scala
@@ -0,0 +1,17 @@
+package com.hewei.contractor
+
+/**
+  *
+  * @author hewei
+  *
+  * @date 16/1/6  11:36
+  *
+  * @version 5.0
+  *
+  * @desc
+  *
+  */
+class TestContractor(xx: Int, oo: Int) {
+  def this(xx: Int) = this(xx, 1)
+  def this()=this(1)
+}
