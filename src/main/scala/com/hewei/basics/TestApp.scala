78dfd8b3df2bd48339b6be8a3d587ebbfca1079e
rjbhewei <rjbhewei@gmail.com>
2016-01-11 14:57:31 +0800

    app test

---
+++ src/main/scala/com/hewei/basics/TestApp.scala
@@ -0,0 +1,17 @@
+package com.hewei.basics
+
+/**
+  *
+  * @author hewei
+  *
+  * @date 16/1/11  14:45
+  *
+  * @version 5.0
+  *
+  * @desc
+  *
+  */
+object TestApp extends App {
+  util.Properties.setProp("scala.time", "true")
+  println("this app extends class")
+}
